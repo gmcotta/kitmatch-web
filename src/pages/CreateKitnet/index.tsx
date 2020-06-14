cbf0a3d6ddc73eb804122e26d58d1b043492fa13
Gustavo Cotta <gmcotta34@gmail.com>
2020-06-14 15:59:44 -0300

    Editorconfig, ESLint and Prettier configuration

--- src/pages/CreateKitnet/index.tsx
+++ src/pages/CreateKitnet/index.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 
 const CreateKitnet: React.FC = () => {
-  return (
-    <h1>Create Kitnet</h1>
-  )
-}
+  return <h1>Create Kitnet</h1>;
+};
 
 export default CreateKitnet;
--- src/pages/SearchKitnet/index.tsx
+++ src/pages/SearchKitnet/index.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 
 const SearchKitnet: React.FC = () => {
-  return (
-    <h1>SearchKitnet</h1>
-  )
-}
+  return <h1>SearchKitnet</h1>;
+};
 
 export default SearchKitnet;
