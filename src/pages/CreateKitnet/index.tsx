c4f48bc9e2860e5bdd0e5cb9fb2e2f7c7148c502
Gustavo Cotta <gmcotta34@gmail.com>
2020-06-14 15:34:49 -0300

    Create home, create and search pages

---
+++ src/pages/CreateKitnet/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const CreateKitnet: React.FC = () => {
+  return (
+    <h1>Create Kitnet</h1>
+  )
+}
+
+export default CreateKitnet;
---
+++ src/pages/SearchKitnet/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SearchKitnet: React.FC = () => {
+  return (
+    <h1>SearchKitnet</h1>
+  )
+}
+
+export default SearchKitnet;
